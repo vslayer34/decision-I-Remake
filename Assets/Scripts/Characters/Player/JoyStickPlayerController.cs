3528711beecb4f8f7976d9fc3a2cc8ab8ef250b1
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2024-05-08 15:19:47 +0300

    Added dynamic joystick to the scene

---
+++ Assets/Scripts/Characters/Player/JoyStickPlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoyStickPlayerController : MonoBehaviour
+{
+    private Vector3 _movementVector;
+
+
+
+    private void Update()
+    {
+        
+    }
+}
