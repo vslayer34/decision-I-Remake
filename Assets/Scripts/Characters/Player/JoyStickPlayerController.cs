b70ee864ae6402cb3925ddbb38eefe49c4d0fc05
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2024-05-08 17:42:13 +0300

    Finally Rotated the player correctly with the joystick position

--- Assets/Scripts/Characters/Player/JoyStickPlayerController.cs
+++ Assets/Scripts/Characters/Player/JoyStickPlayerController.cs
@@ -4,12 +4,47 @@ using UnityEngine;
 
 public class JoyStickPlayerController : MonoBehaviour
 {
-    private Vector3 _movementVector;
+    private Vector3 _orientationVector;
+
+    private CharacterController _characterController;
+
+    [SerializeField]
+    private DynamicJoystick _joyStick;
 
 
+    [SerializeField, Header("Character Properties"), Tooltip("Reference to the rotation speed of the character")]
+    private float _rotationSpeed = 50.0f;
+
+
+
+    // Game Loop Methods---------------------------------------------------------------------------
+    
+    private void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
 
     private void Update()
     {
+        _orientationVector = new Vector3(_joyStick.Direction.x, 0.0f, _joyStick.Direction.y);
+        // Debug.Log(_orientationVector);
+        GetCharacterDirection();
+
+        // transform.Translate(_movementVector * Time.deltaTime * 2);
+    }
+
+    // Member Methods------------------------------------------------------------------------------
+
+    private void GetCharacterDirection()
+    {
+        // Get the angle of the joy stick vectors
+        float headingAngle = Mathf.Atan2(_joyStick.Horizontal, _joyStick.Vertical);
+        Debug.Log(headingAngle * Mathf.Rad2Deg);
+
+        // Take the camera rotation in the calculation wheil removing the x and z axis rotation
+        Quaternion cameraRotation = new Quaternion(0.0f, Camera.main.transform.rotation.y, 0.0f, Camera.main.transform.rotation.w);
         
+        // Apply the rotation to the transform
+        transform.rotation = Quaternion.Euler(0.0f, headingAngle * Mathf.Rad2Deg, 0.0f) * cameraRotation;
     }
 }
