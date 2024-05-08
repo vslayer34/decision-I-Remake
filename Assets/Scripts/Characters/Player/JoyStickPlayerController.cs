5a7a9af8784a1df61fcd85e8f37a73dd0426f626
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2024-05-08 18:03:56 +0300

    Made the character move forward

--- Assets/Scripts/Characters/Player/JoyStickPlayerController.cs
+++ Assets/Scripts/Characters/Player/JoyStickPlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class JoyStickPlayerController : MonoBehaviour
 {
-    private Vector3 _orientationVector;
+    private Vector3 _movementVector;
 
     private CharacterController _characterController;
 
@@ -14,6 +14,9 @@ public class JoyStickPlayerController : MonoBehaviour
 
     [SerializeField, Header("Character Properties"), Tooltip("Reference to the rotation speed of the character")]
     private float _rotationSpeed = 50.0f;
+
+    [SerializeField, Tooltip("The character movement speed")]
+    private float _speed;
 
 
 
@@ -26,9 +29,15 @@ public class JoyStickPlayerController : MonoBehaviour
 
     private void Update()
     {
-        _orientationVector = new Vector3(_joyStick.Direction.x, 0.0f, _joyStick.Direction.y);
+        _movementVector = new Vector3(_joyStick.Direction.x, 0.0f, _joyStick.Direction.y);
+        Debug.Log(_movementVector.sqrMagnitude);
         // Debug.Log(_orientationVector);
-        GetCharacterDirection();
+        
+        if (_movementVector != Vector3.zero)
+        {
+            GetCharacterDirection();
+            MoveCharacter();
+        }
 
         // transform.Translate(_movementVector * Time.deltaTime * 2);
     }
@@ -39,12 +48,17 @@ public class JoyStickPlayerController : MonoBehaviour
     {
         // Get the angle of the joy stick vectors
         float headingAngle = Mathf.Atan2(_joyStick.Horizontal, _joyStick.Vertical);
-        Debug.Log(headingAngle * Mathf.Rad2Deg);
 
         // Take the camera rotation in the calculation wheil removing the x and z axis rotation
         Quaternion cameraRotation = new Quaternion(0.0f, Camera.main.transform.rotation.y, 0.0f, Camera.main.transform.rotation.w);
         
         // Apply the rotation to the transform
         transform.rotation = Quaternion.Euler(0.0f, headingAngle * Mathf.Rad2Deg, 0.0f) * cameraRotation;
+    }
+
+
+    private void MoveCharacter()
+    {
+        _characterController.Move(transform.forward * _movementVector.sqrMagnitude * Time.deltaTime * _speed);
     }
 }
