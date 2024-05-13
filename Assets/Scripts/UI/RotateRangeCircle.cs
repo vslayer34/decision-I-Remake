af92ae0e83ac5c2d8e3fad098a3799f3b158b3bf
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2024-05-13 16:22:59 +0300

    Imported Lean Tween

--- Assets/Scripts/UI/RotateRangeCircle.cs
+++ Assets/Scripts/UI/RotateRangeCircle.cs
@@ -4,14 +4,7 @@ using UnityEngine;
 
 public class RotateRangeCircle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
