f216cbe413e15edde18cd4b3538bf0c401c504c0
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2024-05-13 15:40:22 +0300

    Imported weapon range circle

--- Assets/Scripts/Characters/Player/HandleWeapons.cs
+++ Assets/Scripts/Characters/Player/HandleWeapons.cs
@@ -6,4 +6,13 @@ public class HandleWeapons : MonoBehaviour
 {
     [field: SerializeField, Tooltip("Weapon holding area")]
     public Transform WeaponHoldPoint { get; private set; }
+
+
+
+    // Member Methods------------------------------------------------------------------------------
+
+    private void DisplayRange()
+    {
+        
+    }
 }
